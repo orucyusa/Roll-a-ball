9f57e75b4d408e51783a6864875e0ac0b936eef5
yusaoruc <yusaoruc2@gmail.com>
2021-04-07 13:18:26 +0300

    first commit

---
+++ Assets/Kodlar/TopKontrol.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour
+{
+    float yatay=0;
+    float dikey=0;
+    
+    public int hiz;
+    int sayac=0;
+    public int toplanilacakObjeSayisi;
+    public Text sayacText;
+    public Text oyunBittiText;
+
+    void Start()
+    {
+        Rigidbody fizik= GetComponent<Rigidbody>();
+        
+    }
+
+    void FixedUpdate()
+    {
+        Rigidbody fizik= GetComponent<Rigidbody>();
+        Vector3 vec= new Vector3(yatay,0,dikey);
+
+        fizik.AddForce(vec*hiz);
+        yatay=0;
+        dikey=0;
+    }
+    public void Sbas(){
+        dikey-=1;
+    }
+    public void Wbas(){
+        dikey+=1;
+    }
+    public void Abas(){
+        yatay-=1;
+    }
+    public void Dbas(){
+        yatay+=1;
+    }
+    public void YatayKalk(){
+        yatay=0;
+    }
+    public void DikeyKalk(){
+        dikey=0;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.gameObject.tag=="engel"){
+            Destroy(other.gameObject); 
+            sayac++;
+            sayacText.text="Sayac= "+ sayac;
+            if(sayac==toplanilacakObjeSayisi){
+                    oyunBittiText.text="OYUN BİTTİ";
+            }
+        }
+         
+        
+    }
+}
---
+++ Assets/Kodlar/ToplanacakObje.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToplanacakObje : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Rotate(new Vector3(15,30,45)*Time.deltaTime);
+    }
+}
